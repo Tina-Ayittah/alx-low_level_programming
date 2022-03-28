56e6e72c5581ac1466d472c9f853791dee583e2e
1373733 <dedeboatemaa@gmail.com>
2022-03-28 09:30:30 -0700

    C-even more pointers.....

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,4 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -11,5 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
