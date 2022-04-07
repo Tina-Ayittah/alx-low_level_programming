86713388c8595b6f23497ae0719ec8aef7a16c35
1373733 <dedeboatemaa@gmail.com>
2022-04-07 06:02:57 -0700

    More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,48 +1,107 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+	
 
-#define ERR_MSG "Error"
-
-int _isdigit(char *s);
-
-/**
- * main - takes two numbers as arguments and outputs the product.
- * @argc: the number of arguments including name of program.
- * @argv: an array of arguments
- *
- * Return: 0 if successful.
- */
-int main(int argc, char *argv[])
-{
-	int i;
-	if (argc != 3)
+	/**
+	 * is_digit - checks if a string contains a non-digit char
+	 * @s: string to be evaluated
+	 *
+	 * Return: 0 if a non-digit is found, 1 otherwise
+	 */
+	int is_digit(char *s)
 	{
-		for (i = 0; ERR_MSG[i] != '\0'; i++)
-			_putchar(ERR_MSG[i]);
-		_putchar(10);
+		int i = 0;
+	
+
+		while (s[i])
+		{
+			if (s[i] < '0' || s[i] > '9')
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	
+
+	/**
+	 * _strnum - returns the numgth of a string
+	 * @s: string to evaluate
+	 *
+	 * Return: the numgth of the string
+	 */
+	int _strnum(char *s)
+	{
+		int i = 0;
+	
+
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+		return (i);
+	}
+	
+
+	/**
+	 * errors - handles errors for main
+	 */
+	void errors(void)
+	{
+		printf("Error\n");
 		exit(98);
 	}
-	if (_isdigit(argv[1]) == 0)
-		printf("%s\n", argv[1]);
-		
-	return (0);
-}
+	
 
-/**
- * isdigit - checks if string is a number or not
- * @s: the string to be checked
- *
- * Return: 1 if true, else 0.
- */
-
-int _isdigit(char *s)
-{
-	while (*s)
+	/**
+	 * main - multiplies two positive numbers
+	 * @argc: number of arguments
+	 * @argv: array of arguments
+	 *
+	 * Return: always 0 (Success)
+	 */
+	int main(int argc, char *argv[])
 	{
-		if (*s < 48 || *s > 57)
+		char *s1, *s2;
+		int num1, num2, num, i, carry, digit1, digit2, *result, a = 0;
+	
+
+		s1 = argv[1], s2 = argv[2];
+		if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+			errors();
+		num1 = _strnum(s1);
+		num2 = _strnum(s2);
+		num = num1 + num2 + 1;
+		result = malloc(sizeof(int) * num);
+		if (!result)
 			return (1);
-		s++;
+		for (i = 0; i <= num1 + num2; i++)
+			result[i] = 0;
+		for (num1 = num1 - 1; num1 >= 0; num1--)
+		{
+			digit1 = s1[num1] - '0';
+			carry = 0;
+			for (num2 = _strnum(s2) - 1; num2 >= 0; num2--)
+			{
+				digit2 = s2[num2] - '0';
+				carry += result[num1 + num2 + 1] + (digit1 * digit2);
+				result[num1 + num2 + 1] = carry % 10;
+				carry /= 10;
+			}
+			if (carry > 0)
+				result[num1 + num2 + 1] += carry;
+		}
+		for (i = 0; i < num - 1; i++)
+		{
+			if (result[i])
+				a = 1;
+			if (a)
+				_putchar(result[i] + '0');
+		}
+		if (!a)
+			_putchar('0');
+		_putchar('\n');
+		free(result);
+		return (0);
 	}
-	return (0);
-}
 
