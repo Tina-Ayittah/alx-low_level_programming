4a057b59f4226432dab120bca3ffd23ad0933f45
1373733 <dedeboatemaa@gmail.com>
2022-06-14 03:33:43 -0700

    libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/1373733/alx-low_level_programming/blob/master/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
